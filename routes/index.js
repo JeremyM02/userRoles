d767aec575080dbb360c9464b071f89b53e755fe
JeremyM02 <zom7@fresnou.org>
2022-03-08 08:53:52 -0800

    Slide 61, haven't tested users of other roles.

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,15 @@ router.get('/accounting', pageController.renderAccounting);
 router.get('/marketing', pageController.renderMarketing);
 router.get('/sales',pageController.renderSales);
 router.get('/hr', pageController.renderHR);
+router.get('/profile', pageController.viewProfile);
 
 router.get('/register', userController.renderRegistrationForm);
 router.post('/register', userController.registerUser);
 
 router.get('/login', userController.renderLoginForm);
 router.post('/login', userController.loginUser);
+
+
+router.get('/logout', userController.logout);
 module.exports = router;
 
